5c8fbae44f94fad22388cd53053591800b5e2ac8
folkloren <rhettlrasmussen@gmail.com>
2021-01-19 19:58:45 +0000

    tweaking the dimensions (10" by 5")

--- chart.py
+++ chart.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def graph_to_png(x,y,y_min,y_max,y_label,y_color,title,prefix):
 	plt.clf()
 	plt.cla()
-	plt.figure(figsize=(6,4))
+	plt.figure(figsize=(10,5))
 	plt.plot(x,y,color=y_color)
 	plt.axis([min(x),max(x),y_min,y_max])
 	plt.xlabel('Time in Seconds')
@@ -16,11 +16,12 @@ def graph_to_png(x,y,y_min,y_max,y_label,y_color,title,prefix):
 
 data = []
 
-for i in range(0,12):
+for i in range(7,12):
 	print("Opening data_part_{}.csv".format(i))
 	with open('data_part_{}.csv'.format(i), newline='\r\n') as csvfile:
 		reader = csv.reader(csvfile,delimiter=',',quotechar='"')
-		data.append([])
+		while(len(data) <= i):
+			data.append([])
 		for j,row in enumerate(reader):
 			if j == 0:
 				continue
@@ -29,6 +30,8 @@ for i in range(0,12):
 print("{} csv files processed".format(len(data)))
 
 for chunk in data:
+	if len(chunk) == 0:
+		continue
 	columns = np.transpose(chunk)
 	x = columns[0]
 	gwr = columns[1]
@@ -41,11 +44,4 @@ for chunk in data:
 	graph_to_png(x,g2,0,250,"Gauge Resistance in Ohms","red","Gauge Number 2 Fill Draw Cycles","data_g2_")
 	graph_to_png(x,g3,0,250,"Gauge Resistance in Ohms","green","Gauge Number 3 Fill Draw Cycles","data_g3_")
 
-"""columns = np.transpose(data[0])
-x_axis, y_axis = columns[0], columns[2]
-plt.plot(x_axis,y_axis)
-plt.grid()
-plt.axis([min(x_axis),max(x_axis),min(y_axis),max(y_axis)])
-plt.title('Testing')
-plt.savefig('data_0_GWR.png')
-print('Done!')"""
+print("Done!")
